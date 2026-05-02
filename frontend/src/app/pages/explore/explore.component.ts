74d730925818ea0746b4c9220ca7abd31291cafa
95rishipal <95rishipal@gmail.com>
2026-05-03 01:40:51 +0530

    feat: implement lookbook masonry gallery in explore page

--- frontend/src/app/pages/explore/explore.component.ts
+++ frontend/src/app/pages/explore/explore.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CmsService } from '../../services/cms.service';
 
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.css'
 })
-export class ExploreComponent {
+export class ExploreComponent implements OnInit {
+  lookbookItems: any[] = [];
 
+  constructor(private cmsService: CmsService) {}
+
+  ngOnInit() {
+    this.cmsService.getLookbook().subscribe(data => this.lookbookItems = data);
+  }
 }
