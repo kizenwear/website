88c75a2b0252e80c896ed0b70dde615d3680fa22
95rishipal <95rishipal@gmail.com>
2026-05-03 11:56:03 +0530

    this is dockerised frontend

--- frontend/src/app/pages/explore/explore.component.ts
+++ frontend/src/app/pages/explore/explore.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { CmsService } from '../../services/cms.service';
 
 @Component({
@@ -12,10 +13,17 @@ import { CmsService } from '../../services/cms.service';
 })
 export class ExploreComponent implements OnInit {
   lookbookItems: any[] = [];
+  brandData: any = null;
 
-  constructor(private cmsService: CmsService) {}
+  constructor(private cmsService: CmsService, private http: HttpClient) {}
 
   ngOnInit() {
     this.cmsService.getLookbook().subscribe(data => this.lookbookItems = data);
+    
+    // Fetch data from the Spring Boot backend
+    this.http.get('http://localhost:8080/api/message').subscribe({
+      next: (data) => this.brandData = data,
+      error: (err) => console.error('Failed to fetch brand data from backend', err)
+    });
   }
 }
